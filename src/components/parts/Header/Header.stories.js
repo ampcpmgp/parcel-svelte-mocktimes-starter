a9cb22391b653aab34323436c89434e0d4e3e147
ampcpmgp <ampcpmgp@gmail.com>
2020-11-14 22:43:03 +0900

    upgrade Storybook 6

--- src/components/parts/Header/Header.stories.js
+++ src/components/parts/Header/Header.stories.js
@@ -1,23 +1,27 @@
-import { linkTo } from '@storybook/addon-links'
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Header from './Header.svelte'
 
 export default {
   title: 'Header',
   component: Header,
-  decorators: [withKnobs],
+  argTypes: {
+    onClick: { action: 'onClick' },
+  },
 }
 
-export const Text = () => ({
+const Template = ({ onClick, ...args }) => ({
   Component: Header,
-  props: { text: text('text', 'Text') },
-  on: { click: linkTo('Header', 'Emoji') },
+  props: args,
+  on: {
+    click: onClick,
+  },
 })
 
-export const Emoji = () => ({
-  Component: Header,
-  props: {
-    text: text('text', '😀 😎 👍 💯'),
-  },
-  on: { click: console.info },
-})
+export const Text = Template.bind({})
+Text.args = {
+  text: 'Text',
+}
+
+export const Emoji = Template.bind({})
+Emoji.args = {
+  text: '😀 😎 👍 💯',
+}
